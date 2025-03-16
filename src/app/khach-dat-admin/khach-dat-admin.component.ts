be213c4cdb0254c777be6a898416a0355f0beb76
dangkhoab1910240 <khoab1910240@gmail.com>
2025-03-16 16:57:39 +0700

    fix

--- src/app/khach-dat-admin/khach-dat-admin.component.ts
+++ src/app/khach-dat-admin/khach-dat-admin.component.ts
@@ -10,10 +10,10 @@ import { RegistrationService } from '../Services/registration/registration.servi
 @Component({
   selector: 'app-khach-dat-admin',
   templateUrl: './khach-dat-admin.component.html',
-  styleUrls: ['./khach-dat-admin.component.css']
+  styleUrls: ['./khach-dat-admin.component.css'],
 })
-export class KhachDatAdminComponent implements OnInit{
-  isSidebarOpen: boolean = false; 
+export class KhachDatAdminComponent implements OnInit {
+  isSidebarOpen: boolean = false;
   listDangKy: Registration[] = [];
   eventId: number | string = '';
   userFullname: string = '';
@@ -22,75 +22,84 @@ export class KhachDatAdminComponent implements OnInit{
   dayMonthYear: string | null = null;
   filterByMonth: boolean = false;
   selectedDate: string = '';
-  constructor(
-    private registrationService: RegistrationService
-  ) { }
+  isNhaToChuc: boolean = false;
+  constructor(private registrationService: RegistrationService) {}
   ngOnInit(): void {
+    if (localStorage.getItem('vaiTro') == 'NHA_TO_CHUC') {
+      this.isNhaToChuc = true;
+    }
     this.getAllRegistrations();
   }
   toggleSidebar() {
     this.isSidebarOpen = !this.isSidebarOpen;
   }
   getAllRegistrations() {
-    this.registrationService.getAllRegistrationByFilter(this.eventId,this.userFullname,this.status,this.monthYear,this.dayMonthYear).subscribe({
-      next: (response: Registration[]) => {
-        this.listDangKy = response;
-      },
-      error: (error) => {
-
-      }
-    })
+    this.registrationService
+      .getAllRegistrationByFilter(
+        this.eventId,
+        this.userFullname,
+        this.status,
+        this.monthYear,
+        this.dayMonthYear
+      )
+      .subscribe({
+        next: (response: Registration[]) => {
+          this.listDangKy = response;
+        },
+        error: (error) => {},
+      });
   }
-  updateRegistration(id:number ,trangThai: number) {
+  updateRegistration(id: number, trangThai: number) {
     const object = {
-      status: trangThai
+      status: trangThai,
     };
-    this.registrationService.updateById(id,JSON.parse(localStorage.getItem('userId')!),object).subscribe({
-      next: (response : void)=> {
-        
-        this.getAllRegistrations();
-      },
-      error: (error) => {
-
-      }
-    })
+    this.registrationService
+      .updateById(id, JSON.parse(localStorage.getItem('userId')!), object)
+      .subscribe({
+        next: (response: void) => {
+          this.getAllRegistrations();
+        },
+        error: (error) => {},
+      });
   }
   onStatusChange(value: string) {
     this.status = value ? Number(value) : null;
     this.getAllRegistrations();
-}
+  }
 
-onFilterByMonthChange(event: any) {
+  onFilterByMonthChange(event: any) {
     // Khi checkbox được chọn
     if (this.filterByMonth && this.selectedDate) {
-        const [year, month] = this.selectedDate.split('-');
-        this.monthYear = `${month}/${year}`;
-        this.dayMonthYear = null;
-        console.log(this.dayMonthYear)
+      const [year, month] = this.selectedDate.split('-');
+      this.monthYear = `${month}/${year}`;
+      this.dayMonthYear = null;
+      console.log(this.dayMonthYear);
     } else if (this.selectedDate) {
-        // Khi checkbox không được chọn
-        const [year, month, day] = this.selectedDate.split('-');
-        this.dayMonthYear = `${day}/${month}/${year}`;this.selectedDate;
-        this.monthYear = null;
+      // Khi checkbox không được chọn
+      const [year, month, day] = this.selectedDate.split('-');
+      this.dayMonthYear = `${day}/${month}/${year}`;
+      this.selectedDate;
+      this.monthYear = null;
     }
     this.getAllRegistrations();
-}
+  }
 
-onDateChange(event: any) {
-  console.log(this.selectedDate);
+  onDateChange(event: any) {
+    console.log(this.selectedDate);
     if (this.filterByMonth && this.selectedDate) {
-        const [year, month] = this.selectedDate.split('-');
-        this.monthYear = `${month}/${year}`;
-        this.dayMonthYear = null;
+      const [year, month] = this.selectedDate.split('-');
+      this.monthYear = `${month}/${year}`;
+      this.dayMonthYear = null;
     } else {
       const [year, month, day] = this.selectedDate.split('-');
-      this.dayMonthYear = `${day}/${month}/${year}`;this.selectedDate;
-        this.monthYear = null;
+      this.dayMonthYear = `${day}/${month}/${year}`;
+      this.selectedDate;
+      this.monthYear = null;
     }
     this.getAllRegistrations();
-}
+  }
 
-resetFilters() {
+  resetFilters() {
     this.userFullname = '';
     this.status = null;
     this.monthYear = null;
@@ -98,5 +107,5 @@ resetFilters() {
     this.filterByMonth = false;
     this.selectedDate = '';
     this.getAllRegistrations();
-}
+  }
 }
